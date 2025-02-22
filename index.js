65f39087ee44550bf83d0151cb26f72a5268dadf
MKdivine <jensboerger23@gmail.com>
2025-02-22 23:10:03 +0100

    server working

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express")  
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/CarProject", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+});
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.get("/doge", (req, res) => {
+    res.send("Hello World");
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
