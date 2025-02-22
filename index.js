3a319f329d5daf167804b15fda1de076dcb1c6d6
MKdivine <jensboerger23@gmail.com>
2025-02-22 22:36:25 +0100

    start

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")  
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
